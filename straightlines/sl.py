a48c71f0bc7ccad646cf13230e26dba86f72e916
ctcutler <ctcutler@gmail.com>
2019-02-17 19:30:08 -0500

    draw line from seed

--- straightlines/sl.py
+++ straightlines/sl.py
@@ -6,16 +6,15 @@ def save_image(file_name, image_data):
     img.save(file_name)
 
 def init_solid(width, height, rgb):
-    a = np.array(rgb)
-    tiled = np.tile(a, width * height)
-
+    tiled = np.tile(rgb, width * height)
     return np.reshape(tiled, (height, width, 3))
 
-def draw_line(canvas, start, length, width):
+def draw_line(canvas, start, length, seed):
     c = canvas.copy()
     (x, y) = start
+    width = seed.shape[0]
 
-    c[x:x+length, y:y+width] = np.array((0, 0, 0))
+    c[x:x+length, y:y+width] = seed
 
     return c
 
@@ -24,12 +23,14 @@ def main():
     IMAGE_WIDTH = 16 * SCALE
     IMAGE_HEIGHT = 9 * SCALE
     MARGIN = int(SCALE / 2)
-    WHITE = (255, 255, 255)
+    WHITE = np.array((255, 255, 255))
+    BLACK = np.array((0, 0, 0))
     LINE_WIDTH = 5
 
     solid = init_solid(IMAGE_WIDTH, IMAGE_HEIGHT, WHITE)
+    seed = np.tile(BLACK, LINE_WIDTH).reshape(LINE_WIDTH, BLACK.size)
     first_line = draw_line(
-        solid, (MARGIN, MARGIN), IMAGE_HEIGHT - (MARGIN * 2), LINE_WIDTH
+        solid, (MARGIN, MARGIN), IMAGE_HEIGHT - (MARGIN * 2), seed
     )
     save_image('sl.png', first_line)
 
