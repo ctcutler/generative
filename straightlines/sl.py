7896e515d9c67bb06e9fe0879a7447b527864ca3
ctcutler <ctcutler@gmail.com>
2019-02-17 19:35:11 -0500

    build line separately

--- straightlines/sl.py
+++ straightlines/sl.py
@@ -9,12 +9,13 @@ def init_solid(width, height, rgb):
     tiled = np.tile(rgb, width * height)
     return np.reshape(tiled, (height, width, 3))
 
-def draw_line(canvas, start, length, seed):
+def draw_line(canvas, position, line):
     c = canvas.copy()
-    (x, y) = start
-    width = seed.shape[0]
+    (x, y) = position
+    height = line.shape[0]
+    width = line.shape[1]
 
-    c[x:x+length, y:y+width] = seed
+    c[x:x+height, y:y+width] = line
 
     return c
 
@@ -28,10 +29,10 @@ def main():
     LINE_WIDTH = 5
 
     solid = init_solid(IMAGE_WIDTH, IMAGE_HEIGHT, WHITE)
-    seed = np.tile(BLACK, LINE_WIDTH).reshape(LINE_WIDTH, BLACK.size)
-    first_line = draw_line(
-        solid, (MARGIN, MARGIN), IMAGE_HEIGHT - (MARGIN * 2), seed
-    )
+    height = IMAGE_HEIGHT - (MARGIN * 2)
+    line_flat = np.tile(BLACK, LINE_WIDTH * height)
+    line = line_flat.reshape(height, LINE_WIDTH, BLACK.size)
+    first_line = draw_line(solid, (MARGIN, MARGIN), line)
     save_image('sl.png', first_line)
 
 if __name__== "__main__":
