1f509eb2e7e381dfb8af3984e8eaa172d3c93d15
ctcutler <ctcutler@gmail.com>
2019-02-23 14:53:31 -0500

    more experiementing

--- straightlines/sl.py
+++ straightlines/sl.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 from PIL import Image
 
@@ -19,11 +21,30 @@ def draw_line(canvas, position, line):
 
     return c
 
-def make_line(height, seed):
-    # in addition to experimenting with how quickly the sides fade out,
-    # play with how close the middle is to one side or the other
+def random_walk(how_often, length, minimum, maximum, start):
+    w = []
+    c = start
+    for i in range(length):
+        if random.random() < how_often:
+            if random.random() < .5:
+                c = max(minimum, c - 1)
+            else:
+                c = min(maximum, c + 1)
+        w.append(c)
+    return w
 
-    return np.tile(seed, height).reshape(height, 5, 3)
+def make_line(height, seed):
+    # generate "rotation" offsets
+    #    for every row, .05 chance of either rotating by 1 in either direction
+    # rotate seed as needed while tiling (or after)
+
+    offsets = random_walk(.05, height, 0, 4, 0)
+    line = np.tile(seed, height).reshape(height, 5, 3)
+
+    for i in range(height):
+        line[i] = np.roll(line[i], offsets[i], 0)
+
+    return line
 
 def main():
     SCALE = 85
@@ -65,10 +86,59 @@ def main():
         192, 192, 192,
     ))
 
+    SOFT_LEFT1 = np.array((
+        0, 0, 0,
+        64, 64, 64,
+        96, 96, 96,
+        112, 112, 112,
+        120, 120, 120,
+    ))
+
+    SOFT_LEFT2 = np.array((
+        64, 64, 64,
+        0, 0, 0,
+        64, 64, 64,
+        96, 96, 96,
+        112, 112, 112,
+    ))
+
+    SOFT_LEFT3 = np.array((
+        0, 0, 0,
+        64, 64, 64,
+        96, 96, 96,
+        128, 128, 128,
+        192, 192, 192,
+    ))
+
+    SKINNY1 = np.array((
+        0, 0, 0,
+        0, 0, 0,
+        0, 0, 0,
+        255, 255, 255,
+        255, 255, 255,
+    ))
+
+    SKINNY2 = np.array((
+        0, 0, 0,
+        0, 0, 0,
+        255, 255, 255,
+        255, 255, 255,
+        255, 255, 255,
+    ))
+
+    SKINNY3 = np.array((
+        0, 0, 0,
+        255, 255, 255,
+        255, 255, 255,
+        255, 255, 255,
+        255, 255, 255,
+    ))
+
     line = init_solid(IMAGE_WIDTH, IMAGE_HEIGHT, WHITE)
     height = IMAGE_HEIGHT - (MARGIN * 2)
 
-    for (i, line_seed) in enumerate([HARD, SOFT1, SOFT2, SOFT3]):
+    for (i, line_seed) in enumerate([HARD, SOFT1, SOFT2, SOFT3, SOFT_LEFT1, SOFT_LEFT2,
+        SOFT_LEFT3, SKINNY1, SKINNY2, SKINNY3]):
         line = draw_line(
             line, (MARGIN, (MARGIN * (i+1)) + (5 * i)),
             make_line(height, line_seed)
